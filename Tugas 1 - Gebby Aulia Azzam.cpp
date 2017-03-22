185b6a110e9da837cf84d11b3266f594d27c0874
gebbyazam <azamazamazam9@gmail.com>
2017-03-22 10:37:11 +0700

    Add files via upload

---
+++ Tugas 1 - Gebby Aulia Azzam.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello World" << endl;
+	cout << " Nama : Gebby Aulia Azzam" << endl;
+	cout << " NIM : 16 111 199" << endl;
+	cout << " Kelas : TIF 16 B RM" << endl;
+	return 0;
+}	
